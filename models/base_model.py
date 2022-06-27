fb9017d5abbfd7c11f0db14742c4a9e166695c30
oxyvans <benja.1999.g@hotmail.com>
2022-06-27 16:38:58 +0000

    add task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
- """ base model create """
+""" base model create """
 
 import uuid
-from datetime
+import datetime
 
- class BaseModel()
+class BaseModel():
     """ base modles """
 
-    def __init__(self)
+    def __init__(self):
         """ init method """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()  
         self.updated_at = datetime.datetime.now()
     
-    def __str__(self)
+    def __str__(self):
         """ retrun string """
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
-    
+    def save(self):
+        """ savee """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """ to dict """
+        self.created_at = datetime.datetime.now().isoformat()  
+        self.updated_at = datetime.datetime.now().isoformat()
+        res = self.__dict__
+        res["__class__"] = "BaseModel"
+        return res
