e9f09efe684a608629dcfdeb15a38a854d5d6e9a
SFreire2022 <sebastiancazenave@gmail.com>
2022-07-01 01:48:33 -0300

    Add first commands for the console

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Program that contains the entry point of the command interpreter """
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """ Class definition for holberton AirBnB console """
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """ Quit command to exit from console """
+        exit()
+
+    def do_EOF(self, arg):
+        """ Exit method for EOF """
+        print('')
+        exit()
+
+    def emptyline(self):
+        """ Method to print the prompt again when emptyline entered """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
