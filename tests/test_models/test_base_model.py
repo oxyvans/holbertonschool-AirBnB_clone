c36e4adeeaed9d9ebd7146cf6aa3bc7ddd1d2a95
SFreire2022 <sebastiancazenave@gmail.com>
2022-07-02 19:38:13 -0300

    Add UUID test for base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,6 +8,10 @@ from models.base_model import BaseModel
 
 class TestBaseModule(unittest.TestCase):
     """ A class to test Base Module """
+
+    self.base_model1 = BaseModel()
+    self.base_model2 = BaseModel()
+
     def test_pycodestyle_pep8(self):
         """ Style code meets PEP8 """
         style = pycodestyle.StyleGuide(quit=True)
@@ -15,3 +19,10 @@ class TestBaseModule(unittest.TestCase):
         self.assertEqual(
             check.total_errors, 0,
             'PEP8 style errors: %d' % check.total_errors)
+
+    def test_uuid(self):
+        """ Test uuid """
+        self.assertTrue(hasattr(self.base_model1, "id"))
+        self.assertNotEqual(self.base_model1.id, self.base_model2.id)
+        self.assertEqual(type(self.base_model1.id), str)
+        self.assertEqual(type(self.base_model2.id), str)
