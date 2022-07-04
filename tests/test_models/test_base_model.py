f41eb352dbf87067e709739f3572b25592e257d1
SFreire2022 <sebastiancazenave@gmail.com>
2022-07-04 16:23:41 -0300

    Add more tests for BaseModel Class

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,6 +4,8 @@ Check pycodestyle "PEP8" for the complete module """
 import unittest
 import pycodestyle
 from models.base_model import BaseModel
+import models
+
 
 base_model1 = BaseModel()
 base_model2 = BaseModel()
@@ -25,3 +27,29 @@ class TestBaseModule(unittest.TestCase):
         self.assertNotEqual(base_model1.id, base_model2.id)
         self.assertEqual(type(base_model1.id), str)
         self.assertEqual(type(base_model2.id), str)
+
+    def test_save(self):
+        """ Test save method """
+        old_updete = base_model1.updated_at
+        base_model1.save()
+        new_updete = base_model1.updated_at
+        self.assertTrue(old_updete != new_updete)
+
+    def test_update(self):
+        """ Test update functionality """
+        base_model1.name = "Guillaume"
+        self.assertTrue(hasattr(base_model1, "name"))
+
+    def test_init_kwargs(self):
+        """ Test init with kwargs """
+        base_model1.name = "Guillaume"
+        json_model = base_model1.to_dict()
+        base_model3 = BaseModel(**json_model)
+        self.assertDictEqual(json_model, base_model3.to_dict())
+        self.assertIn("name", base_model3.to_dict())
+        self.assertIsNot(base_model1, base_model3)
+
+    def test_storage(self):
+        """ Test staorage"""
+        objs_dict_rep = models.storage.all()
+        self.assertTrue(objs_dict_rep)
